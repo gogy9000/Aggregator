1f0b36a5411e6629d7a39f693cde090df7cfe307
gogy9000 <goropashnyj9000@gmail.com>
2022-05-11 18:12:43 +0500

    use unit tests for Auth.ts

---
+++ src/Redux/Auth/Auth.test.ts
@@ -0,0 +1,22 @@
+import {actions, authReducer, authStateType} from "./Auth";
+
+
+let state:authStateType
+
+beforeEach(()=>{
+    state={
+        id:null,
+        login:null,
+        email:null,
+        isAuth:false
+    }
+})
+test('authData should be updated',()=>{
+    let action= actions.getAuth('1','azaza','email')
+    let newState= authReducer(state,action)
+    expect(newState.id).toBe('1')
+    expect(newState.email).toBe('email')
+    expect(newState.login).toBe('azaza')
+    expect(newState.isAuth).toBeTruthy()
+
+})
