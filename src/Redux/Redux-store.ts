ad83f698a7b5437cd5b8dd82ec2f6b9f604b64d9
gogy9000 <goropashnyj9000@gmail.com>
2022-05-23 22:41:35 +0500

    use redux-thunk

--- src/Redux/Redux-store.ts
+++ src/Redux/Redux-store.ts
@@ -1,8 +1,9 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import {messagePageReducer} from "./MessagePage/messagePageReducer";
 import {ProfilePageReducer} from "./ProfilePage/ProfilePageReducer";
 import {authReducer} from "./Auth/Auth";
 import {AppReducer} from "./AppReducer/AppReducer";
+import thunk from "redux-thunk";
 
 
 const rootReducer = combineReducers({
@@ -16,5 +17,6 @@ type rootReducerType = typeof rootReducer
 export type AppStateType=ReturnType<rootReducerType>
 export type InferActionsTypes <T> = T extends { [keys: string]: (...args: any[]) => infer U } ? U : never
 
-export let store = createStore(rootReducer)
-// window.store = store
+export let store = createStore(rootReducer,applyMiddleware(thunk))
+// @ts-ignore
+window.store = store
